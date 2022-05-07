918b9fea0cddac4cc15b0be30d1498fe39e7332c
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-05-07 18:43:17 +0300

    update Review

--- app/main/views.py
+++ app/main/views.py
@@ -1,42 +1,51 @@
-
 from flask import render_template,request,redirect,url_for,abort
-from .import main
-from ..requests import get_movie, get_movies,search_movie
-from ..models import Review, User
-from .forms import ReviewForm
-from flask_login import login_required
+from . import main
+from ..requests import get_movies,get_movie,search_movie
 from .forms import ReviewForm,UpdateProfile
-from ..import db,photos
+from ..models import Review,User,PhotoProfile
+from flask_login import login_required
+from .. import db,photos
 
-#Views
+
+
+
+
+
+# Views
 @main.route('/')
 def index():
 
+    '''
+    View root page function that returns the index page and its data
+    '''
 
-
-    #get poplar movie
+    # Getting popular movie
     popular_movies = get_movies('popular')
-    upcoming_movies = get_movies('upcoming')
+    upcoming_movie = get_movies('upcoming')
     now_showing_movie = get_movies('now_playing')
-    
-    title = 'Home - Welcome To Atarah Movie Review '  
+
+    title = 'Home - Welcome to The best Movie Review Website Online'
+
     search_movie = request.args.get('movie_query')
 
     if search_movie:
-        return redirect(url_for('search',movie_name=search_movie))
+        return redirect(url_for('.search',movie_name=search_movie))
     else:
-        return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movies, now_showing = now_showing_movie)   
-    
+        return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
-@main.route('/movie/<int:movie_id>')
-def movie(movie_id):
-    
 
-      movie = get_movie(id)
-      title = f'{movie.title}'
-      reviews = Review.get_reviews(movie.id)
+@main.route('/movie/<int:id>')
+def movie(id):
 
-      return render_template('movie.html',title = title,movie = movie,reviews = reviews)
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    movie = get_movie(id)
+    title = f'{movie.title}'
+    reviews = Review.get_reviews(movie.id)
+
+    return render_template('movie.html',title = title,movie = movie,reviews = reviews)
+
 
 
 @main.route('/search/<movie_name>')
@@ -48,30 +57,31 @@ def search(movie_name):
     movie_name_format = "+".join(movie_name_list)
     searched_movies = search_movie(movie_name_format)
     title = f'search results for {movie_name}'
-    return render_template('search.html',movies = searched_movies)     
+    return render_template('search.html',movies = searched_movies)
 
 
 @main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
 @login_required
 def new_review(id):
 
-    
     form = ReviewForm()
+
     movie = get_movie(id)
 
     if form.validate_on_submit():
         title = form.title.data
         review = form.review.data
+
         new_review = Review(movie.id,title,movie.poster,review)
         new_review.save_review()
-        return redirect(url_for('movie',id = movie.id ))
+
+        return redirect(url_for('.movie',id = movie.id ))
 
     title = f'{movie.title} review'
     return render_template('new_review.html',title = title, review_form=form, movie=movie)
 
-
-
 @main.route('/user/<uname>')
+@login_required
 def profile(uname):
     user = User.query.filter_by(username = uname).first()
 
@@ -91,6 +101,7 @@ def update_profile(uname):
     form = UpdateProfile()
 
     if form.validate_on_submit():
+
         user.bio = form.bio.data
 
         db.session.add(user)
@@ -98,7 +109,8 @@ def update_profile(uname):
 
         return redirect(url_for('.profile',uname=user.username))
 
-    return render_template('profile/update.html',form =form)    
+    return render_template('profile/update.html',form =form)
+
 
 @main.route('/user/<uname>/update/pic',methods= ['POST'])
 @login_required
@@ -108,5 +120,6 @@ def update_pic(uname):
         filename = photos.save(request.files['photo'])
         path = f'photos/{filename}'
         user.profile_pic_path = path
+        user_photo = PhotoProfile(pic_path = path,user = user)
         db.session.commit()
     return redirect(url_for('main.profile',uname=uname))
